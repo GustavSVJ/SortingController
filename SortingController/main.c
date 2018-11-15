cb802214029b9e91d941ae661c8f8f8a30e37774
Gustav Vinther-Jørgensen <gustavsvj@gmail.com>
2018-11-15 08:32:20 +0100

    Added length detection. Not measuring accurately yet

--- SortingController/main.c
+++ SortingController/main.c
@@ -8,6 +8,7 @@
 #include <rtai_lxrt.h>
 #include <pthread.h>
 #include <comedilib.h>
+#include <math.h>
 
 char stopflag = 0;
 
@@ -58,82 +59,127 @@ void *RegulatorThread(void* stuff) {
 	RT_TASK *task = rt_task_init(nam2num("h1901"), 0, 0, 0);
 
 
-	rt_task_make_periodic_relative_ns(task, 0, 5000000);
+	rt_task_make_periodic_relative_ns(task, 0, 2500000);
 
 	statetype shute_reg, belt_reg;
 	regul_init(&shute_reg);
 	regul_init(&belt_reg);
 
 	comedi_t * hw = comedi_open("/dev/comedi2");
-	int i = 0;
+	int j = 0;
+
+	/*Length detection variables*/
+	unsigned int detectorCounter = 0;
+	unsigned int beltSpeedCounter = 0;
+	unsigned int beltSpeed[10];
+	
+	/*End of length detection variables*/
 
 	long long int timeOld = rt_get_time_ns();
+	
 
 	while (stopflag == 0) {
-		long long int timeRelative = rt_get_time_ns() - timeOld;
-		double time = timeRelative / 1000000000.0;
-		//Shute regulator
+
+		/*Length detection*/
+
 		unsigned int data;
-		unsigned int shute_ref = 3200;
+		comedi_data_read_delayed(hw, 0, 2, 0, AREF_DIFF, &data, 50000);
 
-		comedi_data_read_delayed(hw, 0, 0, 0, AREF_DIFF, &data, 50000);
-		fprintf(fp, "%f;%d;",time, data);
-		int e = (shute_ref - data);
-
-		regul_out(&shute_reg, e, shute_b0);
-
-		regul_update(&shute_reg, e, shute_b1, shute_a1);
-
-		if (shute_reg.u > 2045) {
-			data = 4095;
+		if (data > 3000) {
+			detectorCounter++;
 		}
-		else if (shute_reg.u < -2045) {
-			data = 0;
+		else if(detectorCounter != 0) {
+			printf("The counter was %d\n", detectorCounter);
+			double avgBeltSpeed = 0;
+			int i = 0;
+			for (i = 0; i < 10; i++) {
+				avgBeltSpeed += beltSpeed[i];
+			}
+			
+
+			avgBeltSpeed = (((avgBeltSpeed / 10) / 4096) * 20) - 10;
+			
+			avgBeltSpeed = avgBeltSpeed * 384.6153846 * 0.1047197551 * 0.1666666667 * 0.036;
+			
+			double objectLength = detectorCounter * 0.0025 * avgBeltSpeed * 100;
+
+			printf("The average belt speed was %f and the object was %f cm\n", avgBeltSpeed, objectLength);
+
+			detectorCounter = 0;
+		}
+
+		/*End of length detection*/
+
+		if (j) {
+			j = 0;
+
+			long long int timeRelative = rt_get_time_ns() - timeOld;
+			double time = timeRelative / 1000000000.0;
+			//Shute regulator
+			unsigned int data;
+			unsigned int shute_ref = 3200;
+
+			comedi_data_read_delayed(hw, 0, 0, 0, AREF_DIFF, &data, 50000);
+			fprintf(fp, "%f;%d;", time, data);
+			int e = (shute_ref - data);
+
+			regul_out(&shute_reg, e, shute_b0);
+
+			regul_update(&shute_reg, e, shute_b1, shute_a1);
+
+			if (shute_reg.u > 2045) {
+				data = 4095;
+			}
+			else if (shute_reg.u < -2045) {
+				data = 0;
+			}
+			else {
+				data = (unsigned int)shute_reg.u + 2048;
+			}
+
+			comedi_data_write(hw, 1, 0, 0, AREF_GROUND, data);
+			fprintf(fp, "%d;", data);
+			//Belt Regulator
+			comedi_data_read_delayed(hw, 0, 1, 0, AREF_DIFF, &data, 50000);
+			double belt_ref = 175;
+			int inputData = data - 2048;
+			e = (belt_ref - inputData);
+			fprintf(fp, "%d;", data);
+			beltSpeed[beltSpeedCounter] = data;
+			if (beltSpeedCounter < 9) {
+				beltSpeedCounter++;
+			}
+			else {
+				beltSpeedCounter = 0;
+			}
+
+			regul_out(&belt_reg, e, belt_b0);
+
+			regul_update(&belt_reg, e, belt_b1, belt_a1);
+
+			if (belt_reg.u > 2045) {
+				data = 4095;
+			}
+			else if (belt_reg.u < 0) {
+				data = 2048;
+			}
+			else {
+				data = (unsigned int)belt_reg.u + 2048;
+			}
+
+			comedi_data_write(hw, 1, 1, 0, AREF_GROUND, data);
+			fprintf(fp, "%d;\n", data);
 		}
 		else {
-			data = (unsigned int)shute_reg.u + 2048;
+			j = 1;
 		}
 
-		if (i > 200) {
-			//i = 0;
-			//printf("The error is %d and the output is %d and the controller is %f\n", e, data, shute_reg.u);
-		}
-
-		comedi_data_write(hw, 1, 0, 0, AREF_GROUND, data);
-		fprintf(fp, "%d;", data);
-		//Belt Regulator
-		comedi_data_read_delayed(hw, 0, 1, 0, AREF_DIFF, &data, 50000);
-		double belt_ref = 175;
-		int inputData = data - 2048;
-		e = (belt_ref - inputData);
-		fprintf(fp, "%d;", data);
-		regul_out(&belt_reg, e, belt_b0);
-
-		regul_update(&belt_reg, e, belt_b1, belt_a1);
-
-		if (belt_reg.u > 2045) {
-			data = 4095;
-		}
-		else if (belt_reg.u < 0) {
-			data = 2048;
-		}
-		else {
-			data = (unsigned int)belt_reg.u + 2048;
-		}
-
-		if (i > 200) {
-			i = 0;
-			printf("The error is %d and the output is %d and the controller is %f and the input is %d\n", e, data, belt_reg.u, inputData);
-		}
-
-		comedi_data_write(hw, 1, 1, 0, AREF_GROUND, data);
-		fprintf(fp, "%d;\n", data);
 		rt_task_wait_period();
-		i++;
 	}
 
 	comedi_data_write(hw, 1, 1, 0, AREF_GROUND, 2048);
 	comedi_data_write(hw, 1, 0, 0, AREF_GROUND, 2048);
+	comedi_close(hw);
 	fclose(fp);
 	rt_task_delete(task);
 
@@ -177,6 +223,16 @@ int main()
 
 		else if ((strcmp(input, "quit") == 0) || (strcmp(input, "exit") == 0)) {
 			break;
+		}
+
+		else if (strcmp(input, "read") == 0) {
+			comedi_t * hw = comedi_open("/dev/comedi2");
+
+			unsigned int data;
+			comedi_data_read_delayed(hw, 0, 2, 0, AREF_DIFF, &data, 50000);
+			printf("The reading was %d\n", data);
+
+			comedi_close(hw);
 		}
 
 	}
